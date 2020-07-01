001d4c1b594d51f07404f89ffe250127b9e0c3c4
Robert Lathrop <Rizato@users.noreply.github.com>
2020-07-01 12:03:43 -0400

    Return self in __aenter__ (#459)

--- src/polyswarmclient/abstractscanner.py
+++ src/polyswarmclient/abstractscanner.py
@@ -141,6 +141,7 @@ class AbstractScanner:
     async def __aenter__(self):
         if not await self.setup():
             raise ScannerSetupFailedError
+        return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.teardown()
