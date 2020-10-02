05195df6b12606e3b53bb959240a623b4245f701
Rob Lathrop <rl@polyswarm.io>
2020-10-02 15:24:49 -0400

    Whitespace

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -22,6 +22,7 @@ benign_digest = hmac.new(API_KEY.encode('utf-8'), benign_contents.encode('utf-8'
 
 empty_digest = hmac.new(API_KEY.encode('utf-8'), b'', digestmod='sha256').hexdigest()
 
+
 @application.route("/", methods=['POST'])
 def test_receiver():
     body = request.get_json()
